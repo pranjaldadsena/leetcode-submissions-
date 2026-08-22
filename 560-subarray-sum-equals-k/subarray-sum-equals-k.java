e4772b63341a1991caec5b0987120a5ba3aac271
pranjaldadsena <pranjaldadsena@gmail.com>
2026-08-22 12:26:59 +0530

    Time: 25 ms (46.41%) | Memory: 49.7 MB (5.58%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,35 +1,23 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) 
-    {
-        int n = nums.length;
-        int count = 0;
-        int[] prefixSum = new int[n];
-
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < n; i++)
-        {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+    public int subarraySum(int[] nums, int k) {
+      HashMap<Integer, Integer> map = HashMap.newHashMap(nums.length);
+      int[] pr=new int[nums.length];
+      pr[0]=nums[0];
+      int count=0;
+      for(int i=1;i<nums.length;i++){
+        pr[i]=pr[i-1]+nums[i];
+      }
+      for(int i=0;i<nums.length;i++){
+        if(pr[i]==k)count++;
+        int val=pr[i]-k;
+        if(map.containsKey(val)){
+         count+=map.get(val);
         }
-
-        Map<Integer, Integer> m = new HashMap<>(); 
-
-        for (int j = 0; j < n; j++)
-        {
-            if (prefixSum[j] == k) count++;
-
-            int val = prefixSum[j] - k;
-            if (m.containsKey(val))
-            {
-                count += m.get(val);
-            }
-
-            if (!m.containsKey(prefixSum[j]))
-            {
-                m.put(prefixSum[j], 0);
-            }
-            m.put(prefixSum[j], m.get(prefixSum[j]) + 1);
+        if(!map.containsKey(pr[i])){
+         map.put(pr[i],0);
         }
-
-        return count;
+    map.put(pr[i], map.get(pr[i]) + 1);
+      }
+      return count;
     }
 }
