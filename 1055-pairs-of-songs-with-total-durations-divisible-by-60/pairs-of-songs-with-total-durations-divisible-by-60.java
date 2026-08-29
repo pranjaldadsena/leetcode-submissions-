62d641893d63d1e6afd36e512dacc193484b8d81
pranjaldadsena <pranjaldadsena@gmail.com>
2026-08-30 00:50:35 +0530

    Time: 3 ms (91.52%) | Memory: 50 MB (86.64%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,22 +1,13 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        int [] rC=new int[60];
-        int vp=0;
-        for(int i:time){
-            int c=i%60;
-            if(c==0){
-                vp+=rC[0];
-
-            }else{
-                int neededR=60-c;
-                vp+=rC[neededR];
-            }
-            rC[c]++;
+        int[] count = new int[60];
+        int ans = 0;
+        for(int x : time){
+            int rem = x % 60;
+            int need = (60-rem) % 60;
+            ans += count[need];
+            count[rem]++;
         }
-        return vp;
-       
-        
-        
-    }      
-    
+        return ans;
+    }
 }
